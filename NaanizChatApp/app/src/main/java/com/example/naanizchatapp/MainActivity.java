63c1f6c4fecdcd1390d21f106afa68a03e045dfa
AnmolAgarwal141 <39688318+AnmolAgarwal141@users.noreply.github.com>
2020-05-15 09:41:58 +0530

    new builds and changes

---
+++ NaanizChatApp/app/src/main/java/com/example/naanizchatapp/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.naanizchatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        String channelId="channel-01";
+        String channelname="Channel name";
+        int importance=NotificationManager.IMPORTANCE_DEFAULT;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,channelname,importance);
+
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(channel);
+//        }
+            Intent resultIntent = new Intent(this, MainActivity.class);
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setContentTitle("ChatApp")
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText("You have a new message from"))
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(resultPendingIntent);
+            notificationManager.notify(1, builder.build());
+
+
+        }
+    }
+}
